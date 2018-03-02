999431372dd37cc72a758ebab38a0c3f253c9242
Kyeong Lee <kaythechemist@gmail.com>
2018-03-02 16:59:49 +0900

    todo mysum simplified by regular expression

--- play_with_django/todo/views.py
+++ play_with_django/todo/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
 
-def mysum(request, x, y=0, z=0):
-    return HttpResponse(int(x)+int(y)+int(z))
+
+def mysum(request, numbers):
+    # splited = numbers.split('/')
+    # print(splited)
+    number = map(lambda x: int(x or 0), numbers.split('/'))
+    return HttpResponse(sum(number))
