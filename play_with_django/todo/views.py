53af1e7507658a67e21e4f91679cf5656ac9c202
Kyeong Lee <kaythechemist@gmail.com>
2018-03-02 17:11:25 +0900

    todo.views.hello: Korean character matching

--- play_with_django/todo/views.py
+++ play_with_django/todo/views.py
@@ -6,3 +6,6 @@ def mysum(request, numbers):
     # print(splited)
     number = map(lambda x: int(x or 0), numbers.split('/'))
     return HttpResponse(sum(number))
+
+def hello(request, name, age):
+    return HttpResponse('안녕하세요. {}. {}살이시네요.'.format(name, age))
