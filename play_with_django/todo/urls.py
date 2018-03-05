7d1bbec1536f686f0b1fb59f3dfb00089fb28e3c
Kyeong Lee <kaythechemist@gmail.com>
2018-03-05 12:17:29 +0900

    four pattern of function based view(FBV)

--- play_with_django/todo/urls.py
+++ play_with_django/todo/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     url(r'^hello/(?P<name>[ㄱ-힣]+)/(?P<age>\d+)/$', views.hello),
     url(r'^list1/$', views.post_list1),
     url(r'^list2/$', views.post_list2),
+    url(r'^list3/$', views.post_list3),
+    url(r'^excel/$', views.excel_download)
 ]
--- play_with_django/todo/views.py
+++ play_with_django/todo/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse
+import os
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from django.conf import settings
 
 
 def mysum(request, numbers):
@@ -25,3 +27,21 @@ def post_list1(request):
 def post_list2(request):
     name = '공유'
     return render(request, 'todo/post_list.html', {'name': name})
+
+
+def post_list3(request):
+    return JsonResponse({
+        'message': 'hello, python and django',
+        'items': ['Python', 'Django', 'Celery', 'Azure', 'AWS'],
+    }, json_dumps_params={'ensure_ascii': False})
+
+
+def excel_download(request):
+    filepath = os.path.join(settings.BASE_DIR, 'gdplev.xls')
+    # filepath1 = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    # filepath2 = os.path.join(filepath1, 'gdplev.xls')
+    filename = os.path.basename(filepath)
+    with open(filepath, 'rb') as f:
+        response = HttpResponse(f, content_type='application/vnd.ms-excel')
+        response['Content-Disposition'] = 'attachement; filename = "{}"'.format(filename)
+        return response
