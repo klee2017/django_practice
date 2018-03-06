8ca8968bf293738856817d05b1ba1a860e5eccad
Kyeong Lee <kaythechemist@gmail.com>
2018-03-06 15:38:58 +0900

    blog.models.post model.py created/migration migrate/admin register

--- play_with_django/blog/models.py
+++ play_with_django/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100, help_text='please enter the post title. max length : 100 characters.')
+                             # choices=(
+                             #     ('title1', 'title1 label'),
+                             #     ('title2', 'title2 label'),
+                             #     ('title3', 'title3 label'),
+                             # )
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
