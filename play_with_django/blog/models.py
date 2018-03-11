53fb5a2db7e679708e9a529842eafbe87843f753
Kyeong Lee <kaythechemist@gmail.com>
2018-03-11 23:59:18 +0900

    blog.models.Post 모델에 user 필드 추가 및 notebook ep11 내용 추가: related_name > post.comment_set.all

--- play_with_django/blog/models.py
+++ play_with_django/blog/models.py
@@ -1,5 +1,6 @@
 import re
 
+from django.conf import settings
 from django.db import models
 from django.forms import ValidationError
 
@@ -16,7 +17,7 @@ class Post(models.Model):
         ('w', 'Withdrawn')
     )
 
-    author = models.CharField(max_length=20)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
     title = models.CharField(max_length=100, help_text='please enter the post title. max length : 100 characters.')
     content = models.TextField()
     tags = models.CharField(max_length=100, blank=True)
