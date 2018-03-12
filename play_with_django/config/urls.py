487605f00036adacd572b387503230b9969b0c49
Kyeong Lee <kaythechemist@gmail.com>
2018-03-12 18:04:36 +0900

    각 app마다 url namespace를 적용

--- play_with_django/config/urls.py
+++ play_with_django/config/urls.py
@@ -15,19 +15,29 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.shortcuts import redirect
+# from django.views.generic import RedirectView
 
 from config import settings
 
+# def root(request):
+#     return redirect('blog:post_list')
+
+
 urlpatterns = [
+    # url(r'^$', root, name='root'),
+    url(r'^$', lambda r: redirect('blog:post_list'), name='root'),
+    # url(r'^$', RedirectView.as_view(pattern_name='blog:post_list')),
     url(r'^admin/', admin.site.urls),
-    url(r'^blog/', include('blog.urls')),
-    url(r'^todo/', include('todo.urls')),
-    url(r'^accounts/', include('accounts.urls')),
-    url(r'^shop/', include('shop.urls')),
+    url(r'^blog/', include('blog.urls', namespace='blog')),
+    url(r'^todo/', include('todo.urls', namespace='todo')),
+    url(r'^accounts/', include('accounts.urls', namespace='accounts')),
+    url(r'^shop/', include('shop.urls', namespace='shop')),
 ]
 
 if settings.DEBUG:
     import debug_toolbar
+
     urlpatterns += [
         url(r'^__debug__/', include(debug_toolbar.urls)),
     ]
