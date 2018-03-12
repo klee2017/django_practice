43593b9e58dc48ec6734631b04ea4979455136ec
Kyeong Lee <kaythechemist@gmail.com>
2018-03-12 20:03:41 +0900

    blog.views_cbv.post_new 뷰를 통해 CreateView, get_absolute_url 샘플 구현

--- play_with_django/blog/urls.py
+++ play_with_django/blog/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 
+from . import views_cbv
 from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^(?P<id>\d+)/$', views.post_detail, name='post_detail'),
+
+    url(r'^cbv/new/$', views_cbv.post_new),
 ]
