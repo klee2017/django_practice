8d9bd52227924cca77da36dc7098b09c5d0b9cf9
Kyeong Lee <kaythechemist@gmail.com>
2018-03-20 17:03:48 +0900

    media 파일 서빙 지원

--- play_with_django/config/urls.py
+++ play_with_django/config/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.shortcuts import redirect
 # from django.views.generic import RedirectView
@@ -34,6 +35,8 @@ urlpatterns = [
     url(r'^accounts/', include('accounts.urls', namespace='accounts')),
     url(r'^shop/', include('shop.urls', namespace='shop')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
