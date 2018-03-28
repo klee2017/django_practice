2210ea51b0663125ef9cf94e8879ab2350c0b327
Kyeong Lee <kaythechemist@gmail.com>
2018-03-28 19:53:21 +0900

    로그인 추가 옵션:퀴즈를 맞춰야 로그인

--- play_with_django/accounts/forms.py
+++ play_with_django/accounts/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 from .models import Profile
 
@@ -20,3 +20,13 @@ class SignupForm(UserCreationForm):
             address=self.cleaned_data['address'])
 
         return user
+
+
+class LoginForm(AuthenticationForm):
+    answer = forms.IntegerField(label='3+3=?')
+
+    def clean_answer(self):
+        answer = self.cleaned_data.get('answer', None)
+        if answer != 6:
+            raise forms.ValidationError('mismatched!')
+        return answer
